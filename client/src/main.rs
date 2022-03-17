97d0740c47c06d073fe4d2bfc3b12d32d9dee918
Alida <hjelljordalida@gmail.com>
2022-03-17 20:10:37 +0100

    Somewhat of an outline to the client

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,70 @@
+use std::{net::{TcpStream}, io::{Read, Write}, str::from_utf8};
+
+static nodes: &'static [&str] = &["1111", "2222", "3333"];
+
 fn main() {
-    println!("Hello, world!");
+    
+    let is_running = true;
+
+    while is_running {
+        println!("Enter your message:");
+        let message = std::io::stdin().read_line(&mut line).unwrap();
+        let mut msg = [0 as u8; 8];
+        msg = message.to_be_bytes();
+
+        if message.to_string() == "exit" {
+            break;
+        }
+        
+        send(msg, 0)
+    }
+    println!("Terminated.");
+}
+
+// TODO (Maybe): find path
+fn find_path() -> [i32; 3] {
+    let path:[i32; 3] = [1, 2, 3];
+    return path;
+}
+
+// TODO: Write encrypting for path
+fn encrypt(mut msg:[u8; 8]) {
+    let package;
+    for node in nodes {
+    }
+    return package;
+}
+
+// TODO: send to first node in path
+fn send(mut msg:[u8; 8], node:i32) {
+    let addr = format!("localhost:{}", nodes[0]);
+    match TcpStream::connect(addr) {
+        Ok(mut stream) => {
+                stream.write(msg).unwrap();
+                println!("Sent, awaiting reply...");
+
+                // recieve answer:
+                let mut data = [0 as u8; 8]; // using 6 byte buffer
+                match stream.read_exact(&mut data) {
+
+                    // TODO: decrypt
+
+                    Ok(_) => {
+                        if &data == msg {
+                            println!("Recieved: {}", String::from_utf8(data));
+                        } else {
+                            let text = from_utf8(&data).unwrap();
+                            println!("Unexpected reply: {}", text);
+                        }
+                    },
+                    Err(e) => {
+                        println!("Failed to receive data: {}", e);
+                    }
+                }
+            }
+        ,
+        Err(e) => {
+            println!("Failed to connect: {}", e);
+        }
+    }
 }
