9fa708e0b11586de370157627fb6e9cb6cde982e
Erik Borgeteien Hansen <erik@erikbhan.no>
2022-03-17 13:08:52 +0000

    changed server to simple echo server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,40 +1,41 @@
-use std::fs;
-use std::io::prelude::*;
-use std::net::TcpListener;
-use std::net::TcpStream;
+use std::net::{TcpListener, TcpStream};
+use std::thread;
+use std::io::Read;
+use std::io::Write;
 
-
-fn main() {
-    let listener = TcpListener::bind("localhost:7878").unwrap();
-
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-
-        handle_connection(stream);
+fn handle_client(mut stream: TcpStream) {
+    // read 20 bytes at a time from stream echoing back to stream
+    loop {
+        let mut read = [0; 1028];
+        match stream.read(&mut read) {
+            Ok(n) => {
+                if n == 0 { 
+                    // connection was closed
+                    break;
+                }
+                print!("{}", std::str::from_utf8(&read).unwrap());
+                stream.write_all(&read[0..n]).unwrap(); //if problems: remove '_all' from stream.write_all
+            }
+            Err(err) => {
+                panic!("{}", err);
+            }
+        }
     }
 }
 
-fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 1024];
-    stream.read(&mut buffer).unwrap();
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
 
-    let get = b"GET / HTTP/1.1\r\n";
-
-    let (status_line, filename) = if buffer.starts_with(get) {
-        ("HTTP/1.1 200 OK", "index.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
-    };
-
-    let contents = fs::read_to_string(filename).unwrap();
-
-    let response = format!(
-        "{}\r\nContent-Length: {}\r\n\r\n{}",
-        status_line,
-        contents.len(),
-        contents
-    );
-
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                thread::spawn(move || {
+                    handle_client(stream);
+                });
+            }
+            Err(_) => {
+                println!("Error");
+            }
+        }
+    }
 }
