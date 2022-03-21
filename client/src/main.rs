37c0e4c1daba0e043ecbc509d3aae4c253fe707a
Alida <hjelljordalida@gmail.com>
2022-03-21 17:45:43 +0100

    added pathing and fixed errors

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,48 +1,68 @@
-use std::{net::{TcpStream}, io::{Read, Write}, str::from_utf8};
+use std::{net::{TcpStream}, io::{Read, Write, stdin}, str::from_utf8};
+use shuffle::shuffler::Shuffler;
+use shuffle::irs::Irs;
+use rand::rngs::mock::StepRng;
 
-static nodes: &'static [&str] = &["1111", "2222", "3333"];
+static NODES: &'static [&str] = &["1111", "2222", "3333"];
 
 fn main() {
-    
-    let is_running = true;
-
-    while is_running {
+    loop {
         println!("Enter your message:");
-        let message = std::io::stdin().read_line(&mut line).unwrap();
-        let mut msg = [0 as u8; 8];
-        msg = message.to_be_bytes();
+        let mut message = String::new();
+        stdin().read_line(&mut message).expect("Something went wrong when reading message.");
 
-        if message.to_string() == "exit" {
+        if message.trim().eq("exit") {
+            println!("Exiting program.");
             break;
         }
-        
-        // let package = encrypt(msg, find_path())
 
-        send(msg, 0)
+        println!("Sending: {}", message);
+        let mut msg = message.as_bytes();
+
+        let path = find_path();
+
+        // let package = encrypt(msg, path)
+
+        send(msg, path[0])
     }
-    println!("Terminated.");
+    println!("Program terminated.");
 }
 
 // TODO (Maybe): find path
-fn find_path() -> [i32; 3] {
-    let path:[i32; 3] = [1, 2, 3];
+fn find_path() -> Vec<usize> {
+    let mut path = vec![0, 1, 2];
+
+    let mut rng = StepRng::new(2, 13);
+    let mut irs = Irs::default();
+    irs.shuffle(&mut path, &mut rng).expect("Could not shuffle path");
+
     return path;
 }
 
+/*
 // TODO: Write encrypting for path
-fn encrypt(mut msg:[u8; 8], path:[i32; 3]) {
+fn encrypt(mut msg:&[u8], path:[i32; 3]) {
     let package;
     for node in nodes {
     }
     return package;
 }
 
+// TODO: decrypt
+fn decrypt(package:[u8;8]) -> [u8;8] {
+    let response;
+    for node in nodes {
+    }
+    return response;
+}
+*/
+
 // TODO: send to first node in path
-fn send(mut msg:[u8; 8], node:i32) {
-    let addr = format!("localhost:{}", nodes[0]);
+fn send(mut msg:&[u8], node:usize) {
+    let addr = format!("localhost:{}", NODES[node]);
     match TcpStream::connect(addr) {
         Ok(mut stream) => {
-                stream.write(msg).unwrap();
+                stream.write(&msg).unwrap();
                 println!("Sent, awaiting reply...");
 
                 // recieve answer:
@@ -52,8 +72,8 @@ fn send(mut msg:[u8; 8], node:i32) {
                     //let result = decrypt(package);
 
                     Ok(_) => {
-                        if &data == msg {
-                            println!("Recieved: {}", String::from_utf8(data));
+                        if &data == &msg {
+                            println!("Recieved: {:?}", String::from_utf8(data.to_vec()));
                         } else {
                             let text = from_utf8(&data).unwrap();
                             println!("Unexpected reply: {}", text);
@@ -69,12 +89,4 @@ fn send(mut msg:[u8; 8], node:i32) {
             println!("Failed to connect: {}", e);
         }
     }
-}
-
-// TODO: decrypt
-fn decrypt(package:[u8;8]) -> [u8;8] {
-    let response;
-    for node in nodes {
-    }
-    return response;
 }
