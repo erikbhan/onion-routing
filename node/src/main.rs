99fcb08d73709b74018240b6f7a9ba45ce1b23aa
Alida <hjelljordalida@gmail.com>
2022-03-22 11:23:19 +0100

    progression notes

--- node/src/main.rs
+++ node/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
+    // get portnumber from user on start up
+    // set up private key?
+    // listen to port
+    // handle connection
     println!("Hello, world!");
 }
+
+// handle connection:
+// establish tread:
+    // decrypt layer
+    // read header
+    // pass on to destination
+    // wait for response
+    // encrypt response
+    // pass response back to sender
