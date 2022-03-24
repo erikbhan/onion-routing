bcae11db26fdfa209623635eeaba778071878786
Alida <hjelljordalida@gmail.com>
2022-03-24 14:35:34 +0100

    todos, removed code-comments, started get nodes

--- client/src/main.rs
+++ client/src/main.rs
@@ -3,13 +3,9 @@ use tokio::io::AsyncWriteExt;
 use std::error::Error;
 use std::io::Write;
 
-// //use std::io::{Write, stdin, self};
-// use shuffle::shuffler::Shuffler;
-// use shuffle::irs::Irs;
-// use rand::rngs::mock::StepRng;
-
-
 const N: usize = 3; //number of nodes, and therefore keys etc.
+const DA_ADDR: &str = "0.0.0.0";
+const DA_PORT: &str = "8443";
 
 //util method; reads data from the user via stdin and returns immutable string
 //message: a message can be shown to the user before their input
@@ -24,73 +20,51 @@ fn get_user_input(message: &str) -> String {
 }
 
 //asks the DA for nodes; returns an array of nodes where [0] is the entry and [N] is the last
-async fn get_nodes() -> [&'static str; N] {
-    //let da_stream = TcpStream::connect(get_user_input("DA ADDR: "));
-    
-    //TODO: https is important here!!
+async fn get_nodes_and_keys(mut keys: [&str;N], mut nodes: [&str;N]) {
+    let rec = b"GET /nodes/keys HTTPS/1.1";
+    let connector = TlsConnector::builder()
+        .danger_accept_invalid_certs(true)
+        .danger_accept_invalid_hostnames(true)
+        .build().expect("Error when building TLS connection");
 
-    //TODO: client sends "can i haz n nodes" ->
-    // gets n node-addresses over tls ->
-    // parses adresses and returns method
-    
-    //currently just return node-addresses
-    ["localhost:1111", "localhost:2222", "localhost:3333"]
-}
+    let stream = TcpStream::connect(format!("{}:{}", DA_ADDR, DA_PORT)).unwrap();
 
-async fn get_keys(nodes: [&str; N]) -> [&'static str; N] {
-    ["k1", "k2", "k3"]
+    // Domain will be ignored since cert/hostname verification is disabled
+    let mut stream = connector.connect(DA_ADDR, stream).unwrap();
+
+    stream.write_all(rec).unwrap();
+    let mut res = vec![];
+
+    stream.read_to_end(&mut res).expect("Failed to receive live nodes and keys from DA");
+    
+    // TODO: parse nodes and keys from res
+
+    nodes = ["localhost:1111", "localhost:2222", "localhost:3333"];
+    keys = ["k1", "k2", "k3"];
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let nodes: [&str; N] = get_nodes().await;
-    let keys: [&str; N] = get_keys(nodes).await;
+    let mut nodes: [&str; N];
+    let mut keys: [&str; N];
 
-    //TODO: update addr here to entry node when implemented
-    // ((DA IP in config file or...?))
-    let mut connection = TcpStream::connect("127.0.0.1:8080").await?;
+    let mut connection = TcpStream::connect("127.0.0.1:8080").await?; //REMEMBER: update addr here to entry node when implemented
 
     loop {
         let msg = get_user_input("Message: ");
         if msg.eq("exit") {
             break;
         };
-        //msg.encrypt(keys);
+
         connection.write_all(&msg.as_bytes()).await?;
         let buf = read_message_into_buffer(&connection).await;
-        //TODO: translate from buf to text
+        let response = String::from_utf8(buf[0..num_bytes_read].to_vec()).unwrap();
+        print!(response);
     }
     println!("Exiting program...");
     Ok(())
 }
 
-// // TODO: Write encrypting for path
-// fn encrypt(msg:&[u8], path:[i32; 3]) {
-//     let mut package = msg;
-//     for index in path.rev() {
-//         // set HEADER
-//             // destination = "localhost:{}", NODES[index]
-//         // encode package and header
-//             // private_key = KEYS[index]
-//             // send_to_encryption_lib(package, private_key)
-//     }
-//     return package;
-// }
-
-// // TODO: decrypt
-// fn decrypt(package:[u8;8], path:[i32; 3]) -> [u8;8] {
-//     let response;
-//     for index in path {
-//         // decode package
-//             // private_key = KEYS[index]
-//             // send_to_dencryption_lib(package, private_key)
-//     }
-//     return response;
-// }
-
-
-
-//TODO: handle buffer better, rn it returns the buffer lol
 async fn read_message_into_buffer(stream: &TcpStream) -> [u8; 4096] {
     stream.readable().await;
     let mut buf = [0u8; 4096];
@@ -103,39 +77,10 @@ async fn read_message_into_buffer(stream: &TcpStream) -> [u8; 4096] {
             println!("read {} bytes", n);
             return buf;
         }
-        //todo: error handling 😇👼
-        // Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
-        //     continue;
-        // }
         Err(e) => {
-            //return Err(e.into());
-            return buf;
+            print!("An errror occured when recieving respose from server: {}", e.to_string());
+            return [];
         }
     }
 }
 
-// TODO: send to first node in path
-// fn send(msg:&[u8], node:usize) {
-//     let addr = format!("localhost:{}", NODES[node]);
-//     match TcpStream::connect(addr) {
-//         Ok(mut stream) => {
-//             stream.write_all(msg).unwrap();
-//             println!("Sent, awaiting reply...");
-
-//             // recieve answer:
-//             let mut data = [0_u8; 8];
-//             match stream.read_exact(&mut data) {
-//                 Ok(_) => {
-//                     let result = data; // decrypt(data);
-//                     println!("Recieved: {:?}", String::from_utf8(result.to_vec()));
-//                 },
-//                 Err(e) => {
-//                     println!("Failed to receive data: {}", e);
-//                 }
-//             }
-//         },
-//         Err(e) => {
-//             println!("Failed to connect: {}", e);
-//         }
-//     }
-// }
--- dir_auth/src/main.rs
+++ dir_auth/src/main.rs
@@ -22,23 +22,6 @@ fn main() {
     let nodes = Arc::new(Mutex::new(Vec::new()));
     let keys = Arc::new(Mutex::new(Vec::new()));
 
-    fn handle_client(mut stream: TlsStream<TcpStream>, nodes_clone: Arc<Mutex<Vec<String>>>, keys_clone: Arc<Mutex<Vec<String>>>) {
-        let mut buf = [0u8; 4096];
-        let num_bytes_read = stream.read(&mut buf).unwrap();
-        let data = String::from_utf8(buf[0..num_bytes_read].to_vec()).unwrap();
-        let peer_addr = stream.get_ref().peer_addr().unwrap();
-        
-        let mut nodes = nodes_clone.lock().unwrap();
-        let mut keys = keys_clone.lock().unwrap();
-        nodes.push(peer_addr.to_string());
-        keys.push(data);
-        println!("Pushed {} to nodes", peer_addr);
-
-        // Answer incoming stream with ok
-        stream.write_all(b"HTTP/1.1 200 OK\r\n").unwrap();
-        stream.shutdown();
-    }
-
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
@@ -52,8 +35,38 @@ fn main() {
                     }
                 );
             }
-            Err(_e) => { /* connection failed */ }
+            Err(_e) => { 
+                print!("An error occured when receiving requests from node or client: {}", e.to_string());
+            }
         }
         println!("{:?}", nodes);
     }
+}
+
+fn handle_client(mut stream: TlsStream<TcpStream>, nodes_clone: Arc<Mutex<Vec<String>>>, keys_clone: Arc<Mutex<Vec<String>>>) {
+    let mut buf = [0u8; 4096];
+    let num_bytes_read = stream.read(&mut buf).unwrap();
+    let data = String::from_utf8(buf[0..num_bytes_read].to_vec()).unwrap();
+    let peer_addr = stream.get_ref().peer_addr().unwrap();
+
+    // Handle request for nodes from client
+    if data.eq("GET /nodes/keys HTTPS/1.1") {
+        // TODO: make and send JSON array with nodes
+        let node_json = "Coming soon";
+
+        stream.write_all(node_json).unwrap();
+        stream.shutdown();
+
+        return;
+    }
+    
+    let mut nodes = nodes_clone.lock().unwrap();
+    let mut keys = keys_clone.lock().unwrap();
+    nodes.push(peer_addr.to_string());
+    keys.push(data);
+    println!("Pushed {} to nodes", peer_addr);
+
+    // Answer incoming stream with ok
+    stream.write_all(b"HTTP/1.1 200 OK\r\n").unwrap();
+    stream.shutdown();
 }
