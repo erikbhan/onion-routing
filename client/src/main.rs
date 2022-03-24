469df415509cc367c07bff2df5599cc7f4c99810
Erik Borgeteien Hansen <38139009+erikbhan@users.noreply.github.com>
2022-03-24 12:02:18 +0100

    Dir auth (#24)
    * progression notes
    
    * progression notes
    
    * basic layout for main
    
    * port parsing and listener updated
    
    * start implementing crypto
    
    * add https put request
    
    * attempting to make da and node communicate
    
    * add storage of remote addresses to da
    
    Co-authored-by: Alida <hjelljordalida@gmail.com>

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,100 +1,145 @@
-use std::{net::{TcpStream}, io::{Read, Write, stdin}};
-use shuffle::shuffler::Shuffler;
-use shuffle::irs::Irs;
-use rand::rngs::mock::StepRng;
+use openssl::error::ErrorStack;
+use openssl::rsa::Rsa;
+// Async/await
+use tokio::net::TcpStream;
+use tokio::io::AsyncWriteExt;
 
-static NODES: [&str; 3] = ["1111", "2222", "3333"];
-// static KEYS: [&str; 3] = ["key1", "key2", "key3"]
+// STD
+use std::error::Error;
+use std::io::Write;
 
-fn main() {
-    // get_keys();
+// Cryptography
+use openssl::dh::Dh;
+use openssl::pkey::{PKey, Private};
+
+/// Number of nodes/keys to use and get from the DA. (Put in config file?)
+const N: usize = 3;
+
+// TODO: Implement.
+async fn get_nodes() -> [&'static str; N] {
+    ["localhost:1111", "localhost:2222", "localhost:3333"]
+}
+
+// TODO: Implement. Returns array of shared keys with nodes.
+async fn get_keys(nodes: [&str; N]) -> [Result<PKey<Private>, ErrorStack>; N] {
+    let rsa1 = Rsa::generate(2048).unwrap();
+    let rsa2 = Rsa::generate(2048).unwrap();
+    let rsa3 = Rsa::generate(2048).unwrap();
+    let pkey1 = PKey::from_rsa(rsa1);
+    let pkey2 = PKey::from_rsa(rsa2);
+    let pkey3 = PKey::from_rsa(rsa3);
+    [pkey1, pkey2, pkey3]
+}
+
+fn encrypt(data: &str, keys: [PKey<&str>; N]) -> String {
+    let encrypted_data = String::new();
+    for i in (0..N-1).rev() {
+
+    }
+    "encrypted_data".to_string()
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let nodes: [&str; N] = get_nodes().await;
+    //let keys: [&str; N] = get_keys(nodes).await;
+
+    // TODO: connect to entry node, not localhost
+    let mut connection = TcpStream::connect("127.0.0.1:8080").await?;
 
     loop {
-        println!("Enter your message:");
-        let mut message = String::new();
-        stdin().read_line(&mut message).expect("Something went wrong when reading message.");
-
-        if message.trim().eq("exit") {
-            println!("Exiting program.");
+        let msg = get_user_input("Message: ");
+        if msg.eq("exit") {
             break;
+        };
+        //encrypt(msg, keys);
+        connection.write_all(&msg.as_bytes()).await?;
+        let buf = read_message_into_buffer(&connection).await;
+    }
+    println!("Exiting program...");
+    Ok(())
+}
+
+// // TODO: Write encrypting for path
+// fn encrypt(msg:&[u8], path:[i32; 3]) {
+//     let mut package = msg;
+//     for index in path.rev() {
+//         // set HEADER
+//             // destination = "localhost:{}", NODES[index]
+//         // encode package and header
+//             // private_key = KEYS[index]
+//             // send_to_encryption_lib(package, private_key)
+//     }
+//     return package;
+// }
+
+// // TODO: decrypt
+// fn decrypt(package:[u8;8], path:[i32; 3]) -> [u8;8] {
+//     let response;
+//     for index in path {
+//         // decode package
+//             // private_key = KEYS[index]
+//             // send_to_dencryption_lib(package, private_key)
+//     }
+//     return response;
+// }
+
+async fn read_message_into_buffer(stream: &TcpStream) -> [u8; 4096] {
+    stream.readable().await;
+    let mut buf = [0u8; 4096];
+    match stream.try_read(&mut buf) {
+        Ok(0) => {
+            println!("Stream closed");
+            return buf;
         }
-
-        println!("Sending: {}", message);
-        let msg = message.as_bytes();
-
-        let path = find_path();
-
-        let package = msg; // encrypt(msg, path);
-
-        send(package, path[0])
+        Ok(n) => {
+            println!("read {} bytes", n);
+            return buf;
+        }
+        //todo: error handling 😇👼
+        // Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
+        //     continue;
+        // }
+        Err(e) => {
+            //return Err(e.into());
+            return buf;
+        }
     }
-    println!("Program terminated.");
 }
-
-// TODO (Maybe): find path
-fn find_path() -> Vec<usize> {
-    let mut path = vec![0, 1, 2];
-
-    let mut rng = StepRng::new(2, 13);
-    let mut irs = Irs::default();
-    irs.shuffle(&mut path, &mut rng).expect("Could not shuffle path");
-
-    path
-}
-
-/*
-// TODO: Handshake nodes to get 
-fn get_keys() {
-    for node in NODES
-}
-
-// TODO: Write encrypting for path
-fn encrypt(msg:&[u8], path:[i32; 3]) {
-    let mut package = msg;
-    for index in path.rev() {
-        // set HEADER
-            // destination = "localhost:{}", NODES[index]
-        // encode package and header
-            // private_key = KEYS[index]
-            // send_to_encryption_lib(package, private_key)
-    }
-    return package;
-}
-
-// TODO: decrypt
-fn decrypt(package:[u8;8], path:[i32; 3]) -> [u8;8] {
-    let response;
-    for index in path {
-        // decode package
-            // private_key = KEYS[index]
-            // send_to_dencryption_lib(package, private_key)
-    }
-    return response;
-}
-*/
 
 // TODO: send to first node in path
-fn send(msg:&[u8], node:usize) {
-    let addr = format!("localhost:{}", NODES[node]);
-    match TcpStream::connect(addr) {
-        Ok(mut stream) => {
-            stream.write_all(msg).unwrap();
-            println!("Sent, awaiting reply...");
+// fn send(msg:&[u8], node:usize) {
+//     let addr = format!("localhost:{}", NODES[node]);
+//     match TcpStream::connect(addr) {
+//         Ok(mut stream) => {
+//             stream.write_all(msg).unwrap();
+//             println!("Sent, awaiting reply...");
 
-            // recieve answer:
-            let mut data = [0_u8; 8];
-            match stream.read_exact(&mut data) {
-                Ok(_) => {
-                    let result = data; // decrypt(data);
-                    println!("Recieved: {:?}", String::from_utf8(result.to_vec()));
-                },
-                Err(e) => {
-                    println!("Failed to receive data: {}", e);
-                }
-            }
-        },
-        Err(e) => {
-            println!("Failed to connect: {}", e);
-        }
-    }
+//             // recieve answer:
+//             let mut data = [0_u8; 8];
+//             match stream.read_exact(&mut data) {
+//                 Ok(_) => {
+//                     let result = data; // decrypt(data);
+//                     println!("Recieved: {:?}", String::from_utf8(result.to_vec()));
+//                 },
+//                 Err(e) => {
+//                     println!("Failed to receive data: {}", e);
+//                 }
+//             }
+//         },
+//         Err(e) => {
+//             println!("Failed to connect: {}", e);
+//         }
+//     }
+// }
+
+/// Returns a String given by the user through stdin. Optional message prompt.
+fn get_user_input(message: &str) -> String {
+    print!("{}", message);
+    std::io::stdout().flush(); // Force print by flushing
+    let mut input = String::new();
+    std::io::stdin()
+        .read_line(&mut input)
+        .expect("Error when reading user input from stdin");
+    input.trim().to_string()
 }
--- node/src/main.rs
+++ node/src/main.rs
@@ -1,3 +1,57 @@
+extern crate native_tls;
+
+use native_tls::{ TlsConnector };
+use std::io::{Read, Write};
+use std::net::{ TcpListener, TcpStream} ;
+
+const DA_ADDR: &str = "0.0.0.0";
+const DA_PORT: &str = "8443";
+const PORT: &str = "3000";
+
+fn get_key_and_send_to_da() -> [u8; 32] {
+    let key = b"an example very very secret key."; //32 byte
+    
+    let connector = TlsConnector::builder()
+        .danger_accept_invalid_certs(true)
+        .danger_accept_invalid_hostnames(true)
+        .build().expect("Error when building TLS connection");
+
+    let stream = TcpStream::connect(format!("{}:{}", DA_ADDR, DA_PORT)).unwrap();
+
+    // Domain will be ignored since cert/hostname verification is disabled
+    let mut stream = connector.connect(DA_ADDR, stream).unwrap();
+
+    stream.write_all(key).unwrap();
+    let mut res = vec![];
+
+    // TODO: Error if not 200 OK
+    stream.read_to_end(&mut res).unwrap();
+    println!("{}", String::from_utf8_lossy(&res));
+    *key
+}
+
 fn main() {
-    println!("Hello, world!");
+    let key = get_key_and_send_to_da();
+
+    let listener = TcpListener::bind(format!("localhost:{}", PORT)).unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    let num_bytes_read = stream.read(&mut buffer).unwrap();
+    let data = std::str::from_utf8(&buffer[0..num_bytes_read]).unwrap();
+
+    //For now, print received data
+    println!("{}", data);
+
+    // Answer incoming stream
+    let res_ok = b"HTTP/1.1 200 OK\r\n";
+    let num_read_bytes = stream.write(res_ok).unwrap();
+    stream.flush().unwrap();
 }
