a02abe40a058bd444794f3400b099b3835884176
Erik Borgeteien Hansen <erik@erikbhan.no>
2022-03-24 10:59:52 +0000

    add storage of remote addresses to da

--- dir_auth/src/main.rs
+++ dir_auth/src/main.rs
@@ -4,11 +4,10 @@ use native_tls::{Identity, TlsAcceptor, TlsStream};
 use std::fs::File;
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 use std::thread;
 
 fn main() {
-    // TODO: Create a better cert
     let mut file = File::open("foo.p12").unwrap();
     let mut pkcs12 = vec![];
     file.read_to_end(&mut pkcs12).unwrap();
@@ -19,28 +18,42 @@ fn main() {
 
     let listener = TcpListener::bind("0.0.0.0:8443").unwrap();
 
-    // TODO: Error handling
-    fn handle_client(mut stream: TlsStream<TcpStream>) {
-        loop {
-            let mut buf = [0u8; 4096];
-            let num_bytes_read = stream.read(&mut buf).unwrap();
-            let data = std::str::from_utf8(&buf[0..num_bytes_read]).unwrap();
-            println!("{}", data);
-            stream.write_all(b"HTTP/1.1 200 OK").unwrap();
-        }
+    // Type alias?
+    let nodes = Arc::new(Mutex::new(Vec::new()));
+    let keys = Arc::new(Mutex::new(Vec::new()));
+
+    fn handle_client(mut stream: TlsStream<TcpStream>, nodes_clone: Arc<Mutex<Vec<String>>>, keys_clone: Arc<Mutex<Vec<String>>>) {
+        let mut buf = [0u8; 4096];
+        let num_bytes_read = stream.read(&mut buf).unwrap();
+        let data = String::from_utf8(buf[0..num_bytes_read].to_vec()).unwrap();
+        let peer_addr = stream.get_ref().peer_addr().unwrap();
+        
+        let mut nodes = nodes_clone.lock().unwrap();
+        let mut keys = keys_clone.lock().unwrap();
+        nodes.push(peer_addr.to_string());
+        keys.push(data);
+        println!("Pushed {} to nodes", peer_addr);
+
+        // Answer incoming stream with ok
+        stream.write_all(b"HTTP/1.1 200 OK\r\n").unwrap();
+        stream.shutdown();
     }
 
-    // TODO: Graceful shutdown
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
                 let acceptor = acceptor.clone();
-                thread::spawn(move || {
-                    let stream = acceptor.accept(stream).unwrap();
-                    handle_client(stream);
-                });
+                let nodes_clone = Arc::clone(&nodes);
+                let keys_clone = Arc::clone(&keys);
+                thread::spawn(
+                    move || {
+                        let stream = acceptor.accept(stream).unwrap();
+                        handle_client(stream, nodes_clone, keys_clone);
+                    }
+                );
             }
             Err(_e) => { /* connection failed */ }
         }
+        println!("{:?}", nodes);
     }
 }
--- node/src/main.rs
+++ node/src/main.rs
@@ -2,24 +2,24 @@ extern crate native_tls;
 
 use native_tls::{ TlsConnector };
 use std::io::{Read, Write};
-use std::net::TcpStream;
-use aes_gcm::{Aes256Gcm, Key, Nonce}; // Or `Aes128Gcm`
-use aes_gcm::aead::{Aead, NewAead};
+use std::net::{ TcpListener, TcpStream} ;
 
-fn init() {
+const DA_ADDR: &str = "0.0.0.0";
+const DA_PORT: &str = "8443";
+const PORT: &str = "3000";
+
+fn get_key_and_send_to_da() -> [u8; 32] {
     let key = b"an example very very secret key."; //32 byte
     
-    // TODO: Error handling
-    // TODO: Cert verification    
     let connector = TlsConnector::builder()
         .danger_accept_invalid_certs(true)
         .danger_accept_invalid_hostnames(true)
-        .build().unwrap();
+        .build().expect("Error when building TLS connection");
 
-    let stream = TcpStream::connect("localhost:8443").unwrap();
+    let stream = TcpStream::connect(format!("{}:{}", DA_ADDR, DA_PORT)).unwrap();
 
     // Domain will be ignored since cert/hostname verification is disabled
-    let mut stream = connector.connect("localhost", stream).unwrap();
+    let mut stream = connector.connect(DA_ADDR, stream).unwrap();
 
     stream.write_all(key).unwrap();
     let mut res = vec![];
@@ -27,118 +27,31 @@ fn init() {
     // TODO: Error if not 200 OK
     stream.read_to_end(&mut res).unwrap();
     println!("{}", String::from_utf8_lossy(&res));
+    *key
 }
 
 fn main() {
-    init();
-    // TODO: Find better way to handle key (generate on startup).
-    let key = Key::from_slice(b"an example very very secret key.");
-    let cipher = Aes256Gcm::new(key);
+    let key = get_key_and_send_to_da();
 
-    // TODO: Gen new nonce for every message with a cryptographically secure random byte generator
-    // TODO: Nonce has to be stored; it is needed for decryption (I think?).
-    let nonce = Nonce::from_slice(b"unique nonce"); // 96-bits; unique per message
-
-    let ciphertext = cipher.encrypt(nonce, b"plaintext message".as_ref())
-        .expect("encryption failure!"); // NOTE: handle this error to avoid panics!
-
-    let plaintext = cipher.decrypt(nonce, ciphertext.as_ref())
-        .expect("decryption failure!"); // NOTE: handle this error to avoid panics!
-
-    assert_eq!(&plaintext, b"plaintext message");
-    println!("{}", std::str::from_utf8(&plaintext).unwrap());
-}
-
-
-
-/*
-async fn send_public_key_to_da(aes_key: &[u8; 16]) -> Result<(), Box<dyn std::error::Error + Send + Sync>>  {
-    let mut body = String::from(r#"{"library":""#);
-    body.push_str(std::str::from_utf8(aes_key).unwrap());
-    body.push_str(r#"hyper"}"#);
-    let req = Request::builder()
-        .method(Method::PUT)
-        .uri("localhost:8443") // TODO: DA's address
-        .header("content-type", "application/json")
-        .body(Body::from(body))?;
-
-    let https = HttpsConnector::new();
-    let client = Client::builder()
-        .build::<_, hyper::Body>(https);
-
-    let res = client.request(req).await?;
-
-    println!("Response: {}", res.status());
-
-    Ok(())
-}
-
-async fn prepare_keys() -> &'static str {
-    let buf = b"ABCDABCDABCDABCD";
-    //let mut buf = [0u8; 16];
-    //let mut iv = [0u8; 16];
-    //rand_bytes(&mut buf).unwrap();
-    //rand_bytes(&mut iv).unwrap();
-    //let aes_key = AesKey::new_encrypt(&buf).unwrap();
-    send_public_key_to_da(buf).await;
-    std::str::from_utf8(buf).unwrap()
-}
-
-#[tokio::main]
-async fn main() {
-    let aes_key = prepare_keys().await;
-
-    /*
-    // get portnumber from user on start up
-    //let addr = local_ip().unwrap();
-    //println!("Enter port 1111, 2222 or 3333:");
-    /*let mut port = String::new();
-    match stdin().read_line(&mut port) {
-        Ok(_) => {
-            let port = port.trim();
-            let port_ok = port == "1111" || port == "2222" || port == "3333";
-            if port_ok {
-                println!("Starting Onion-node with adress {}:{}", addr, port)
-            } else {
-                panic!("Port number not valid.")
-            }
-        },
-        Err(e) => {
-            panic!("Failed to read port: {}", e);
-        }
-    }*/
-    // get private key from enum? based on nr
-    let node_id = port.substring(0, 1).parse::<i32>().unwrap();
-    let private_key = format!("temp key {}", node_id);
-    // listen to port
-    let full_addr = format!("127.0.0.1:{}", port.trim());
-
-    let listener = TcpListener::bind(full_addr).expect("Could not establish listener!");
+    let listener = TcpListener::bind(format!("localhost:{}", PORT)).unwrap();
 
     for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                thread::spawn(move || {
-                    // handle connection
-                    handle_request(stream);
-                });
-            }
-            Err(_) => {
-                println!("Error");
-            }
-        }
-    }*/
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
 }
 
-// handle connection:
-//fn handle_request(mut stream: TcpStream) {
-    // decrypt layer
-    // read header
-    // pass on to destination
-    // wait for response
-    // encrypt response
-    // pass response back to sender
-//}
-    
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    let num_bytes_read = stream.read(&mut buffer).unwrap();
+    let data = std::str::from_utf8(&buffer[0..num_bytes_read]).unwrap();
 
-    */
+    //For now, print received data
+    println!("{}", data);
+
+    // Answer incoming stream
+    let res_ok = b"HTTP/1.1 200 OK\r\n";
+    let num_read_bytes = stream.write(res_ok).unwrap();
+    stream.flush().unwrap();
+}
